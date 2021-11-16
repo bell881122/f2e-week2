b2a6cd6cdd961ea0d1a78a1946cc5d3a6899e6dd
emi <bell881122@gmail.com>
2021-11-16 23:17:20 +0800

    取得 TDX 資料

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 
-import Button from '@mui/material/Button';
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import Button from '@mui/material/Button'
+// Custom
+import { getAuthorizationHeader } from 'src/service/_config'
+//--------------------
 function App() {
+  useEffect(() => {
+    (async () => await axios({
+      method: 'get',
+      url: `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/Taichung?$top=12&$format=JSON`,
+      headers: getAuthorizationHeader()
+    }))().then(res => {
+      console.log(res)
+    }).catch(err => console.log(err))
+  }, []);
   return (
     <div>
       <Button variant="contained">Contained</Button>
