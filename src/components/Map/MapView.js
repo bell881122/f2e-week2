584b9231baefa47e3765eb7dbf63b76f888352cb
emi <bell881122@gmail.com>
2021-11-17 23:26:20 +0800

    add flyto

--- src/components/Map/MapView.js
+++ src/components/Map/MapView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
 import {
     MapContainer,
@@ -6,17 +6,37 @@ import {
     Marker,
     Polyline,
     Popup,
+    useMap,
 } from 'react-leaflet';
 // Custom
 import VenueLocationIcon from 'src/components/Map/VenueLocationIcon';
 import 'src/components/Map/MapView.css'
 //--------------------
 
+function SetFlyto({ flyto }) {
+    const map = useMap();
+
+    useEffect(() => {
+        if (map && flyto) {
+            map.flyTo(flyto, map.getZoom())
+        }
+    }, [map, flyto]);
+
+    return null
+}
+
 export default function CyclingMap({ markers, polyline }) {
+    const [flyto, setflyto] = useState();
     const center = [25.04795444238345, 121.51693473083246]; // 台北車站
 
+    useEffect(() => {
+        if (polyline)
+            setflyto(polyline[0])
+    }, [polyline]);
+
     return (
-        <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={center} zoom={15}>
+            {flyto && <SetFlyto flyto={flyto} />}
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
