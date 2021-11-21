353fed30626213af0007248f9f8044e91b4fcbad
emi <bell881122@gmail.com>
2021-11-21 23:07:05 +0800

    add MUI、增添首頁版型

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+// MUI
+import CssBaseline from '@mui/material/CssBaseline'
+// Custom
+import Logo from 'src/asset/logo.png'
+import 'src/App.scss'
+//--------------------
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <CssBaseline />
+      <header>
+        <div className="wrap">
+          <h1>
+            <span style={{ display: 'none' }}>台灣自行車旅遊網</span>
+            <img src={Logo} alt="logo" />
+          </h1>
+          <p className="menu">找路線</p>
+        </div>
+      </header>
+      <main className="wrap">
+        App
+      </main>
+    </>
   );
 }
 
