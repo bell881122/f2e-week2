be3769f9d159fdd506ac6667e639e56d1536d9f7
emi <bell881122@gmail.com>
2021-11-21 23:06:25 +0800

    Reset Project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import React from 'react'
-// MUI
-import CssBaseline from '@mui/material/CssBaseline';
-// Custom
-import Cycling from 'src/components/Cycling';
-import 'src/App.css'
-//--------------------
 function App() {
   return (
-    <div style={{padding:'10px', width:'100wh',height:'100vh'}}>
-      <CssBaseline />
-      <Cycling />
+    <div>
+      App
     </div>
   );
 }
--- src/_config.js
+++
@@ -1,5 +0,0 @@
-export const serviceStatusConfig = [
-    { color: 'red', text: '停止營運' },
-    { color: 'green', text: '正常營運' },
-    { color: 'yellow', text: '暫停營運' },
-];
